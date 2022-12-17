2b0943c8d1d7795c77ebeb064ae40f8c1cdec071
ccwCCW95 <ccw15111840702@gmail.com>
2022-12-17 15:46:33 +1000

    20221217 Commit 用户登录功能 解决中文乱码

--- src/main/java/com/ccw/reggie/controller/EmployeeController.java
+++ src/main/java/com/ccw/reggie/controller/EmployeeController.java
@@ -1,10 +1,18 @@
 package com.ccw.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.ccw.reggie.common.R;
+import com.ccw.reggie.entity.Employee;
 import com.ccw.reggie.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @RestController
@@ -13,4 +21,42 @@ public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
+
+    /**
+     * 员工登录
+     * @param request
+     * @param employee
+     * @return
+     */
+    @PostMapping("/login")
+    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee){
+
+        // 1. 对拿到的密码进行加密
+        String password = employee.getPassword();
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
+
+        // 2. 根据页面提交的username查询数据库
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<Employee>();
+        queryWrapper.eq(Employee::getUsername, employee.getUsername());
+        Employee emp = employeeService.getOne(queryWrapper);
+
+        // 3. 如果没有查询到 返回失败
+        if(emp == null){
+            return R.error("登录失败！");
+        }
+
+        // 4. 比对密码
+        if(!emp.getPassword().equals(password)){
+            return R.error("密码错误！");
+        }
+
+        // 5. 查看员工状态
+        if(emp.getStatus() == 0){
+            return R.error("员工已被禁用！");
+        }
+
+        // 6. 登录成功 放入session
+        request.getSession().setAttribute("employee", emp.getId());
+        return R.success(emp);
+    }
 }
