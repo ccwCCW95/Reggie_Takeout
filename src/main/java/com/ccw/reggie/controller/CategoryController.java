1722ea57abcb0bcfdf5fb8a18f1215433ec6dc96
ccwCCW95 <ccw15111840702@gmail.com>
2022-12-21 21:44:48 +1000

    20221221 Commit 菜品分类 分页查询

--- src/main/java/com/ccw/reggie/controller/CategoryController.java
+++ src/main/java/com/ccw/reggie/controller/CategoryController.java
@@ -1,14 +1,13 @@
 package com.ccw.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.ccw.reggie.common.R;
 import com.ccw.reggie.entity.Category;
 import com.ccw.reggie.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 分类管理
@@ -30,5 +29,26 @@ public class CategoryController {
         log.info("category:{}",category);
         categoryService.save(category);
         return R.success("新增分类成功！");
+    }
+
+    /**
+     * 分页查询
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize){
+        // 分页构造器
+        Page<Category> pageInfo = new Page<Category>(page, pageSize);
+        // 条件构造器
+        LambdaQueryWrapper<Category> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        // 排序
+        lambdaQueryWrapper.orderByAsc(Category::getSort);
+
+        // 进行分页查询
+        categoryService.page(pageInfo, lambdaQueryWrapper);
+
+        return R.success(pageInfo);
     }
 }
